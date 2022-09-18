8ad0371bfb7e66c45fe116f93b8e6f4c1394cb78
Anuj Aggarwal <anuj010787@gmail.com>
2022-09-18 14:43:00 +0530

    Use View Component and Post comment and write the total count and use  VS 2022

---
+++ Lab2_DataPassing/Lab2_DataPassing/Components/CommentViewComponent.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Lab2_DataPassing.Components
+{
+    public class CommentViewComponent : ViewComponent
+    {
+        static List<string> history = new List<string>();
+
+        public CommentViewComponent()
+        {
+
+        }
+
+        //public async Task<IViewComponentResult> InvokeAsync()
+        //{
+        //    var models = history;
+        //    return await Task.FromResult((IViewComponentResult)View("AddComment",models));
+        //}
+
+        public IViewComponentResult Invoke(string comment)
+        {
+            if(!string.IsNullOrEmpty(comment))
+                history.Add(comment);
+
+            ViewData["CommentCount"] = history.Count;
+            return View("~/Views/Components/_Comments.cshtml", history);
+        }
+    }
+}
---
+++ Lab2_DataPassing/Lab2_DataPassing/Controllers/ArticleController.cs
@@ -0,0 +1,23 @@
+﻿using Lab2_DataPassing.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Lab2_DataPassing.Controllers
+{
+    public class ArticleController : Controller
+    {
+        public IActionResult CommentHistory()
+        {
+            return View();
+        }
+
+        public IActionResult PostComment(ArticleModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                ViewData["comment"] = model.Comments;
+            }
+
+            return View("~/Views/Article/CommentHistory.cshtml");
+        }
+    }
+}
